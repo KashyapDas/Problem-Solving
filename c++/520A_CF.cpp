0c10f0d4c576def65e51fafebd43cf5c030b689f
DBi06 <divyambajpai06@gmail.com>
2025-11-05 21:44:48 +0530

    Solve Codeforces

---
+++ c++/520A_CF.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <unordered_set>
+
+using namespace std;
+
+int main() {
+    int n;
+    string s;
+    cin >> n;
+    cin >> s;
+    string sub = "abcdefghijklmnopqrtsuvwxyz";
+
+
+    for(int i = 0; i < n; i++){
+    }
+
+    return 0;
+}
