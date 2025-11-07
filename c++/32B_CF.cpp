5519939774a8b24c1231ba75a92b5be6b3432ba5
DBi06 <divyambajpai06@gmail.com>
2025-11-08 00:04:57 +0530

    solved codeforces

---
+++ c++/32B_CF.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	string a;
+	cin >> a;
+	int l = a.length();
+	
+	for(int i = 0; i < l; i++)
+	{
+		if(a[i] == '.')
+		{
+			cout << 0;
+		}
+		else if(a[i] == '-' && a[i+1] == '.')
+		{
+			cout << 1;
+			i++;
+		}
+		else if(a[i] == '-' && a[i+1] == '-')
+		{
+			cout << 2;
+			i++;
+		}
+	}
+	
+	
+	return 0;
+	
+}
